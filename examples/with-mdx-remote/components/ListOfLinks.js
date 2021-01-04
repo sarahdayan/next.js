48b857c2944687467ec3f082f90ddf50f9c5e4ab
Sarah Dayan <sarah.dayan@algolia.com>
2021-01-04 17:17:24 +0100

    feat: add summary in list of links

--- examples/with-mdx-remote/components/ListOfLinks.js
+++ examples/with-mdx-remote/components/ListOfLinks.js
@@ -5,15 +5,17 @@ export function connectListOfLinks(data) {
     return (
       <ul>
         {links.map((link) => {
-          const { title, url } = link
+          const { title, url, description } = link
           const href = url || link
           const name = title || data[link]?.data?.title || href
+          const summary = description || data[link]?.data?.description || null
 
           return (
             <li key={href}>
               <Link href={`/${href}`}>
                 <a>{name}</a>
               </Link>
+              {summary && <p>{summary}</p>}
             </li>
           )
         })}
